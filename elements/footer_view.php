7261b63d8a52c2fe6e331b6ce7aa4feb4db385f2
Muhammad Ehtisham <hard.coded321@gmail.com>
2013-03-07 15:02:26 +0500

    all files deleted

--- elements/footer_view.php
+++
@@ -1,7 +0,0 @@
-<!--============================Footer============================-->
-<div id="footer">
-	<div class="wrapper">
-	Copyright (C) 2009    TheNation - Admin Panel. All Rights Reserved. Powered by <a href="http://bramerz.pk/">BRAMERZ</a>.
-	</div>
-</div>
-<!--End Footer-->
--- layouts/epaper_template_bottom_view.php
+++
@@ -1,3 +0,0 @@
-	</body>
-	<!--End Body-->
-</html>
